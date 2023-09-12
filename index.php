581e639aa86bb502a124c669f75cec63cca9e88a
Tim8PPLG <144774112+Tim8PPLG@users.noreply.github.com>
2023-09-12 14:15:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Contoh list dengan PHP</title>
+   </head>
+<body>
+<h2>Daftar absensi mahasiswa</h2>
+   <o1>
+      <?php
+      for ($i= 1; $i <= 1000; $i++)
+         {
+           echo  "<li>Nama mahasiswa ke-$i</li>";
+         }
+       ?>
+    </o1
+</body>
+</html>
